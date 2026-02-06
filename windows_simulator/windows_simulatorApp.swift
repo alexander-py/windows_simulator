4ab3206047e5be08447b2fffa136c58434838440
alexander-py <alexanderphoenixbusiness@mail.bg>
2026-02-06 23:31:07 +0200

    Initial Commit

---
+++ windows_simulator/windows_simulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  windows_simulatorApp.swift
+//  windows_simulator
+//
+//  Created by Alexander Stefanov on 6.02.26.
+//
+
+import SwiftUI
+
+@main
+struct windows_simulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
